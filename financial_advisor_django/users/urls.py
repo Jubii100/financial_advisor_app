6ff4b9537b91f680ae9f47483760630d3fbbdb37
Jubii100 <90710349+Jubii100@users.noreply.github.com>
2022-04-30 23:34:15 +0400

    clean_commit

--- financial_advisor_django/users/urls.py
+++ financial_advisor_django/users/urls.py
@@ -2,15 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('use-profile/',
-         views.UserProfileDetailView.as_view(), name='user_profile'),
-    path('user-profile-update/', views.UserProfileUpdateView.as_view(),
-         name='user_profile_update'),
-    # path('user_profile_create/',),
-    path('create-users/', views.UserCreateView.as_view(), name='create_user'),
-    # path('test', views.test, name='test'),
-    path('API-create-user/', views.UserCreateAPI.as_view(), name='API_create_user'),
-    path('API-user-profile-update/',
+    path('user/', views.UserCreateAPI.as_view(), name='API_create_user'),
+    path('user/profile/',
          views.UserProfileUpdateAPI.as_view(), name='API_user_profile_update'),
 
 ]
--- financial_advisor_django/users/views.py
+++ financial_advisor_django/users/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User as auth_user
-from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -9,14 +6,7 @@ from rest_framework import authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from .forms import UserModelForm
-from .forms import RegisterForm
 from .models import UserProfile
-from django.views.generic import (
-    CreateView,
-    DetailView,
-    UpdateView,
-)
 
 
 class UserCreateAPI(ObtainAuthToken):
@@ -49,50 +39,3 @@ class UserProfileUpdateAPI(APIView):
                                                                 user=request.user)
         profile.save()
         return Response('profile updated')
-
-
-class UserCreateView(CreateView):
-    template_name = 'users/user_create.html'
-    form_class = RegisterForm
-    # queryset = auth_user.objects.all()  # <blog>/<modelname>_list.html
-    success_url = '/users/user_profile_update/'
-
-    def form_valid(self, form):
-        print(form.cleaned_data)
-        return super().form_valid(form)
-
-    # def get_success_url(self):
-    #    return '/'
-
-
-class UserProfileDetailView(DetailView):
-    template_name = 'users/user_detail_profile.html'
-    queryset = UserProfile.objects.all().values(
-        'name', 'interests', 'hobbies', 'age', 'gender', 'net_worth')
-
-    def get_object(self):
-        logedin_user = self.request.user
-        return get_object_or_404(UserProfile, user=logedin_user)
-
-    @ method_decorator(login_required(login_url='/login/'))
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
-
-class UserProfileUpdateView(UpdateView):
-    template_name = 'users/user_profile_update.html'
-    form_class = UserModelForm
-    queryset = UserProfile.objects.all().values(
-        'name', 'interests', 'hobbies', 'age', 'gender', 'net_worth')  # <blog>/<modelname>_list.html
-
-    def get_object(self):
-        logedin_user = self.request.user
-        return get_object_or_404(UserProfile, user=logedin_user)
-
-    def form_valid(self, form):
-        print(form.cleaned_data)
-        return super().form_valid(form)
-
-    @ method_decorator(login_required(login_url='/login/'))
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
